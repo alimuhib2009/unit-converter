b70a1f2f16a67ccb2c1e95a0c1635da013d91601
Ali Muhib <160390031+alimuhib2009@users.noreply.github.com>
2025-02-26 19:13:53 -0800

    Add files via upload

--- converter.py
+++ converter.py
@@ -8,8 +8,12 @@ st.header("UNIT CONVERTER")
 
 conversionType = st.selectbox(label='Choose Unit Type:', options=["Length", "Temperature", "Area", "Weight", "Time"])
 
+#Select Unit
+
 if conversionType == "Length" :
-    selectUnit = st.selectbox(label='select Unit', options=["Meter", "Inch", "Centimeter"])
+    selectUnit = st.selectbox(label='select Unit', options=["Meter", "Inch", "Centimeter", "Kilometer", "Milimeter", "Micrometer", "Nanometer", "Mile", "Yard", "Foot"])
 elif conversionType == "Temperature":
     selectUnit = st.selectbox(label='Select Unit', options=["celsius", "kelvin", "Fahrenheit"])
+elif conversionType == "Area":
+    selectUnit = st.selectbox(label='select Unit', options=["Square Meter", "Square Kilometer", "Square Centimeter", "Square Milimeter", "Square Micrometer", "Hectare", "Square Mile", "Square Yard", "Square Foot", "Square Inch", "Arce"])
 
