dc9ea8f2712e05b96f6035a4e9c0d5329c588cc7
Ali Muhib <160390031+alimuhib2009@users.noreply.github.com>
2025-02-26 20:40:24 -0800

    Add files via upload

--- converter.py
+++ converter.py
@@ -27,17 +27,19 @@ value = st.number_input(label='from' , min_value = 0, max_value = 999999999)
 
 #To Unit
 
+def Converter():
+    if conversionType == "Length" :
+        if fromUnit == "Meter":
+            ToUnit = st.selectbox(label='To', options=["Meter", "Inch", "Centimeter", "Kilometer", "Milimeter", "Micrometer", "Nanometer", "Mile", "Yard", "Foot"])
+            if ToUnit == "Centimeter":
+                 resultValue = value * 100
+    elif conversionType == "Temperature":
+        ToUnit = st.selectbox(label='To', options=["celsius", "kelvin", "Fahrenheit"])
+    elif conversionType == "Area":
+        ToUnit = st.selectbox(label='To', options=["Square Meter", "Square Kilometer", "Square Centimeter", "Square Milimeter", "Square Micrometer", "Hectare", "Square Mile", "Square Yard", "Square Foot", "Square Inch", "Arce"])
 
-if conversionType == "Length" :
-    ToUnit = st.selectbox(label='To', options=["Meter", "Inch", "Centimeter", "Kilometer", "Milimeter", "Micrometer", "Nanometer", "Mile", "Yard", "Foot"])
-elif conversionType == "Temperature":
-    ToUnit = st.selectbox(label='To', options=["celsius", "kelvin", "Fahrenheit"])
-elif conversionType == "Area":
-    ToUnit = st.selectbox(label='To', options=["Square Meter", "Square Kilometer", "Square Centimeter", "Square Milimeter", "Square Micrometer", "Hectare", "Square Mile", "Square Yard", "Square Foot", "Square Inch", "Arce"])
 
 
-if fromUnit == "Meter" == ToUnit =="Centimeter" :
-    resultValue = value * 100
 
 
     st.number_input(label="To", value = resultValue)
