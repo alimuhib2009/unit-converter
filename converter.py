0af43bdf1ba162cbc77550f649848207154f8e89
Ali Muhib <160390031+alimuhib2009@users.noreply.github.com>
2025-02-26 20:21:40 -0800

    Add files via upload

--- converter.py
+++ converter.py
@@ -8,24 +8,45 @@ st.header("UNIT CONVERTER")
 
 conversionType = st.selectbox(label='Choose Unit Type:', options=["Length", "Temperature", "Area", "Weight", "Time"])
 
-#Select Unit
+#Select Unit 
 
 if conversionType == "Length" :
-    selectUnit = st.selectbox(label='From', options=["Meter", "Inch", "Centimeter", "Kilometer", "Milimeter", "Micrometer", "Nanometer", "Mile", "Yard", "Foot"])
+    fromUnit = st.selectbox(label='From', options=["Meter", "Inch", "Centimeter", "Kilometer", "Milimeter", "Micrometer", "Nanometer", "Mile", "Yard", "Foot"])
 elif conversionType == "Temperature":
-    selectUnit = st.selectbox(label='From', options=["celsius", "kelvin", "Fahrenheit"])
+    fromUnit = st.selectbox(label='From', options=["celsius", "kelvin", "Fahrenheit"])
 elif conversionType == "Area":
-    selectUnit = st.selectbox(label='From', options=["Square Meter", "Square Kilometer", "Square Centimeter", "Square Milimeter", "Square Micrometer", "Hectare", "Square Mile", "Square Yard", "Square Foot", "Square Inch", "Arce"])
+    fromUnit = st.selectbox(label='From', options=["Square Meter", "Square Kilometer", "Square Centimeter", "Square Milimeter", "Square Micrometer", "Hectare", "Square Mile", "Square Yard", "Square Foot", "Square Inch", "Arce"])
 
 
 
 
+# here we type value
 value = st.number_input(label='from' , min_value = 0, max_value = 999999999)
 
 
 
-if selectUnit == "Meter":
-    resultValue = value * 100  #centi
-    st.number_input(label="To" , value=resultValue )
+#To Unit
+
+
+if conversionType == "Length" :
+    ToUnit = st.selectbox(label='From', options=["Meter", "Inch", "Centimeter", "Kilometer", "Milimeter", "Micrometer", "Nanometer", "Mile", "Yard", "Foot"])
+elif conversionType == "Temperature":
+    ToUnit = st.selectbox(label='From', options=["celsius", "kelvin", "Fahrenheit"])
+elif conversionType == "Area":
+    ToUnit = st.selectbox(label='From', options=["Square Meter", "Square Kilometer", "Square Centimeter", "Square Milimeter", "Square Micrometer", "Hectare", "Square Mile", "Square Yard", "Square Foot", "Square Inch", "Arce"])
+
+
+if fromUnit == "Meter" == ToUnit =="Centimeter" :
+    resultValue = value * 100
+
+
+
+
+
+
+
+
+    
+
 
 
